08d34ce3fa8ecb603d617c43dd99095c4e7e7ff7
AmzalFoumi <147682418+AmzalFoumi@users.noreply.github.com>
2025-06-28 10:53:15 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Jobs
+        </a>
+      </section>
+    </>
+  );
+};
+
+export default App;
+
+//Some code previously used to get a feel of JSX
+// import React from 'react'
+
+// const App = () => {
+// const name = "John";
+// const names = ['Brad', 'Jimi', 'kaml', 'damar'];
+// const loggedIn = true;
+// const styles = {
+//   color:'red',
+//   fontSize:'15px',
+// }
+
+//   return (
+//     // <div>App</div>
+//     <>
+//      <div classNameName="bg-blue-500 text-white p-8 text-center"></div>
+//       <h1 classNameName="text-4xl font-bold">Hello {name} v4!</h1>
+//       <p classNameName="mt-4">This uses the default font.</p>
+//       <ul>
+//         {names.map((name, index) =>(
+//           <li key={index} style={styles} >{name}</li>
+//         ))}
+//       </ul>
+//       {loggedIn && <h1>Hello Member</h1>}
+//     </>
+//   )
+// }
+
+// export default App
