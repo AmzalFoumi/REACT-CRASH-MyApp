691dfe0cab4ccbee48e8b791dc9c7cb9ba814d36
AmzalFoumi <147682418+AmzalFoumi@users.noreply.github.com>
2025-07-02 15:34:26 +0530

    start  using json server

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,10 +1,30 @@
-import React from "react";
-import jobs from "../jobs.json";
+import React, { useEffect, useState } from "react";
+
 import JobListingSingular from "./JobListingSingular";
+import Spinner from "./Spinner";
 
 const JobListings = ({ isHome = false }) => {
-  //Dynamically choosing to show only 3 jobs if its home page, and showing all jobs if not
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  // //Dynamically choosing to show only 3 jobs if its home page, and showing all jobs if not
+  // const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
 
   console.log(jobs);
   return (
@@ -16,11 +36,21 @@ const JobListings = ({ isHome = false }) => {
             {/* Dynamically loading a title */}
             {isHome ? "Recent Jobs" : "Browse Jobs"}
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {jobListings.map((job) => (
-              <JobListingSingular key={job.id} job={job} />
-            ))}
-          </div>
+          {/* 
+              Conditional rendering based on loading state:
+              If loading is true, show a "Loading" message.
+              Once data is fetched (loading is false), 
+              map over the jobs array and render JobListingSingular for each job.
+            */}
+          {loading ? (
+            <Spinner loading={loading} />
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {jobs.map((job) => (
+                <JobListingSingular key={job.id} job={job} />
+              ))}
+            </div>
+          )}
         </div>
       </section>
     </>
